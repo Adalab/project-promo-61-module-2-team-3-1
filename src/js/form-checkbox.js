ee0a6b545ae178a5704571e71d7ca5fe6606c921
CarmenDaluz <daluuz.fernandez.carmen@gmail.com>
2026-01-12 20:24:58 +0100

    creating checkbox, choosing background, let's continue

--- src/js/form-checkbox.js
+++ src/js/form-checkbox.js
@@ -2,10 +2,20 @@
 
 
 const selectCheckbox = document.querySelector('.form-checkbox');
-const addPhotoDOM = document.querySelector('.background1');
+const changeURLPhoto = document.querySelector('.background1');
+const checkOption1 = document.getElementById('input-option1');
+const checkOption2 = document.getElementById('input-option2');
+const checkOption3 = document.getElementById('input-option3');
 
 
-selectCheckbox.addEventListener("click", (e) => {
-    addPhotoDOM.classList.add("background2");
-    console.log('funciona');
+selectCheckbox.addEventListener("change", (e) => {
+    if (checkOption1.checked) {
+       changeURLPhoto.setAttribute('src', '/images/photo-sushi-blur.png'); 
+    } else if (checkOption2.checked) {
+       changeURLPhoto.setAttribute('src', '/images/photo-food-blur.png'); 
+    } else if (checkOption3.checked) {
+        changeURLPhoto.setAttribute('src', '/images/photo-cupcake-blur.png');
+    }
+    
+    
 })
