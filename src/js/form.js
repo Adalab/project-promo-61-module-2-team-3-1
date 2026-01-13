e5288683cd77ddd9c7d4fd0b64883631058202c9
CarmenDaluz <daluuz.fernandez.carmen@gmail.com>
2026-01-13 19:36:27 +0100

    change things

--- src/js/form.js
+++ src/js/form.js
@@ -10,6 +10,7 @@ input.addEventListener('input', manejarInput);
 });
 
 function manejarInput(event) {
+  
   const input = event.currentTarget;
   const campoResultado = document.getElementById(input.dataset.id);
   campoResultado.textContent = input.value;
