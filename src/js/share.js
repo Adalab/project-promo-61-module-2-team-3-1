a4ba7fc89e1710949081d96c2da5dabed96318b6
Tu Nombre <tu-email@ejemplo.com>
2026-01-14 17:34:35 +0100

    nothing

--- src/js/share.js
+++ src/js/share.js
@@ -1,11 +1,6 @@
-'use strict';
+const panel = document.querySelector(".panel");
+const header = document.querySelector(".panel-header");
 
-document.addEventListener("DOMContentLoaded", () => {
-  const panel = document.querySelector(".panel");
-  const header = panel.querySelector(".panel-header");
-
-  header.addEventListener("click", () => {
-    panel.classList.toggle("open");
-    console.log("click"); // para comprobar que funciona
-  });
+header.addEventListener("click", () => {
+  panel.classList.toggle("open");
 });
